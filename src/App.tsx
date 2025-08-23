eebe333f214cb2cace74c452a644fb09f4c4b9c8
Max Zakharzhevskiy <maxzz@msn.com>
2025-08-23 14:32:17 -0700

    WIP. fix ts types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { UISymbolDefs } from "./components/ui/icons/symbols";
+
 export function App() {
     return (
         <div className="min-h-screen bg-slate-100 grid place-items-center">
+            <UISymbolDefs />
             Start
         </div>
     );
