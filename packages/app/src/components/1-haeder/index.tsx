20fe29b886de478f28772d8bf2fe00e8c0bfa800
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-01 15:50:34 -0700

    WIP. generator. fix header section layout

--- packages/app/src/components/1-haeder/index.tsx
+++ packages/app/src/components/1-haeder/index.tsx
@@ -2,11 +2,12 @@ import { SymbolFire } from "../ui/icons/symbols";
 
 export function SectionHeader() {
     return (
-        <header className="px-4 w-full h-8 bg-background-light flex items-center justify-between">
+        <header className="px-4 w-full h-8 bg-background-light border-b border-sky-200 shadow flex items-center gap-1">
             <div className="flex items-center gap-2">
-                <SymbolFire className="size-6 text-red-600 [--fill-a:var(--color-hi)]" />
+                {/* <SymbolFire className="size-6 text-amber-200 [--fill-a:var(--color-hi)]" /> */}
+                <SymbolFire className="size-6 text-sky-500 [--fill-a:var(--color-sky-400)] opacity-25" />
             </div>
-            <div className="text-xs flex items-center gap-2">
+            <div className="text-xs font-semibold uppercase text-sky-700/25">
                 SVG Symbol Icons Tester
             </div>
         </header>
